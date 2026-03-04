8fc47b6cd87e6ab978873ec6a7a199b1cd4e6481
kjaredic <kjaredic@protonmail.com>
2026-03-04 12:11:33 +0100

    remove unused constant

--- contracts/Constants.sol
+++ contracts/Constants.sol
@@ -8,8 +8,6 @@ uint256 constant MAX_FEE_PIPS = 1_00_00;
 uint256 constant PIPS = 1_00_00_00;
 
 /// @dev permit constants
-string constant PERMIT_TRANSFER_FROM_WITNESS_TYPEHASH_STUB = "PermitWitnessTransferFrom(TokenPermissions permitted,address spender,uint256 nonce,uint256 deadline,";
-
 string constant MAKER_WITNESS_TYPE_STRING = "MakerSwapIntent witness)MakerSwapIntent(address counterparty,address inputToken,address outputToken,uint256 inputAmount,uint256 outputAmount,bool unwrap)TokenPermissions(address token,uint256 amount)";
 bytes32 constant MAKER_WITNESS_TYPEHASH = keccak256("MakerSwapIntent(address counterparty,address inputToken,address outputToken,uint256 inputAmount,uint256 outputAmount,bool unwrap)");
 
